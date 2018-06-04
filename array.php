e164a9c06b4ef880439b0e088fedcc5e293fae08
Anuar Bilyalov <anuarbilyalov1@gmail.com>
2018-06-04 20:38:10 +0600

    first project

---
+++ array.php
@@ -0,0 +1,8 @@
+<?php
+$arr = array("somearray"=> array(
+    array ('123','456'),
+    array ('key1'=>'789','qwe'),
+    array ('rty','uio')
+));
+echo $arr[2][0];
+?>
---
+++ cart.js
@@ -0,0 +1,1 @@
+fetch('goods.json').
